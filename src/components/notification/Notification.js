762b96a5ac85dcfd0260e60a767663edba208fd9
Wojciech <wojciechonoszko@gmail.com>
2022-06-17 21:47:18 +0200

    updating

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Notification = ({message}) => <p>{message}</p>;
+export default function Notification({ message }) {
+    return <p>{message}</p>
+
+}
+
+//const Notification = ({message}) => <p>{message}</p>;
 
 Notification.propTypes = {
     message: PropTypes.string.isRequired
 };
 
-export default Notification;
+//export default Notification;
--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Section = ({children, title}) => (
-    <section>
+export default function Section({ title, children }){
+    return (
+        <section>
         <h2 className="sectionTitle">{title}</h2>
         {children}
     </section>
-);
+    )
+}
+
+// const Section = ({children, title}) => (
+//     <section>
+//         <h2 className="sectionTitle">{title}</h2>
+//         {children}
+//     </section>
+// );
 
 Section.propTypes = {
     title: PropTypes.string.isRequired
 };
 
-export default Section;
+//export default Section;
