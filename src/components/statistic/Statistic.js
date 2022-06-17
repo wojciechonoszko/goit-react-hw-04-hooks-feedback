53c0745bd1595cf08a1781cd2a9d4a1ed6639895
Wojciech <wojciechonoszko@gmail.com>
2022-06-17 22:02:04 +0200

    update

--- src/components/statistic/Statistic.js
+++ src/components/statistic/Statistic.js
@@ -2,25 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {StatisticElem} from './StatisticStyles';
 
-const Statistic = ({options, feedback, total, positivePercentage}) => {
+export default function Statistics({
+    good,
+    bad,
+    neutral,
+    positivePercentage,
+    total,
+}) {
     return (
-        <>
-            {options.map(option => (
-                <StatisticElem key={option}>
-                    {option}: {feedback[option]}
-                </StatisticElem>
-            ))}
-            <p>Total: {total}</p>
-            <p>Positive feedback: {positivePercentage}%</p>
-        </>
+        <StatisticElem>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral} </li>
+          <li>Bad: {bad} </li>
+          <li>Total: {total} </li>
+          <li>
+            Positive feedback:
+            {total > 0 && positivePercentage}%
+          </li>
+        </StatisticElem>
     );
 }
 
-Statistic.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string.isRequired),
-    feedback: PropTypes.object.isRequired,
+Statistics.propTypes = {
+    good: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired,
     total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired
-};
+  };
+    
 
-export default Statistic;
+// const Statistic = ({options, feedback, total, positivePercentage}) => {
+//     return (
+//         <>
+//             {options.map(option => (
+//                 <StatisticElem key={option}>
+//                     {option}: {feedback[option]}
+//                 </StatisticElem>
+//             ))}
+//             <p>Total: {total}</p>
+//             <p>Positive feedback: {positivePercentage}%</p>
+//         </>
+//     );
+// }
+
+// Statistic.propTypes = {
+//     options: PropTypes.arrayOf(PropTypes.string.isRequired),
+//     feedback: PropTypes.object.isRequired,
+//     total: PropTypes.number.isRequired,
+//     positivePercentage: PropTypes.number.isRequired
+// };
+
+// export default Statistic;
--- src/components/statistic/StatisticStyles.js
+++ src/components/statistic/StatisticStyles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
-export const StatisticElem = styled.p`
+export const StatisticElem = styled.ul`
     text-transform: capitalize;
+    list-style: none;
 `;
